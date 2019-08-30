d8eb76cd6e3312ea050c0702835e464ce9628e42
Kevin Locke <kevin@kevinlocke.name>
2019-08-30 16:29:19 -0600

    Create .eslintrc for this project
    Style JavaScript like JSON so that code can be more easily moved between
    them.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+
+"use strict";
+
+module.exports = {
+  "extends": "./node",
+
+  // Use style consistent with JSON for easier sharing between formats
+  "rules": {
+    "comma-dangle": ["error", "never"],
+    "max-len": "off",
+    "quotes": ["error", "double"],
+    "quote-props": ["error", "always"]
+  }
+};
