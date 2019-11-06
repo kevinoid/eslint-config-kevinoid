64aa36ee3a68b83fbe90e661965ff1ab54ade1d5
Kevin Locke <kevin@kevinlocke.name>
2019-11-06 11:42:19 -0700

    fixup! Move IE 11-specific unicorn rules to separate file
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- ie11.js
+++ ie11.js
@@ -161,22 +161,6 @@ module.exports = {
     "prefer-const": ["error", {
       "destructuring": "any",
       "ignoreReadBeforeAssign": true
-    }],
-
-
-    // ---------------------------------------------------------------------------
-    // Unicorn Rules <https://github.com/sindresorhus/eslint-plugin-unicorn#rules>
-    // ---------------------------------------------------------------------------
-    // don't require the exponentiation operator over Math.pow()
-    // since IE 11 doesn't support the exponentiation operator
-    "unicorn/prefer-exponentiation-operator": "off",
-
-    // don't require .flatMap() over .map().flat()
-    // since IE 11 doesn't support .flatMap()
-    "unicorn/prefer-flat-map": "off",
-
-    // don't require String#startsWith() & String#endsWith() over alternatives
-    // since IE 11 doesn't support startsWith/endsWith
-    "unicorn/prefer-starts-ends-with": "off"
+    }]
   }
 };
