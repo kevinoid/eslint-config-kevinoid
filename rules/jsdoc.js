99bcfe8f1aff5aaad5f733ec449cff2655ddf617
Kevin Locke <kevin@kevinlocke.name>
2020-05-29 12:12:48 -0600

    feat: don't require @param/@returns for @private
    For short methods which aren't exported these are often superfluous.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- rules/jsdoc.js
+++ rules/jsdoc.js
@@ -10,6 +10,18 @@ module.exports = {
 
   "rules": {
     // Don't require JSDoc comments on classes and functions.
-    "jsdoc/require-jsdoc": "off"
+    "jsdoc/require-jsdoc": "off",
+
+    // Require @param for all parameters, except with @inheritdoc or @private.
+    // Note: Would prefer to require for @private if any other tags present.
+    "jsdoc/require-param": ["error", {
+      "exemptedBy": ["inheritdoc", "private"]
+    }],
+
+    // Require @returns, except with @inheritdoc or @private.
+    // Note: Would prefer to require for @private if any other tags present.
+    "jsdoc/require-returns": ["error", {
+      "exemptedBy": ["inheritdoc", "private"]
+    }]
   }
 };
