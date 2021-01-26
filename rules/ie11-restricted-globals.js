2ab4b1eda1a9edc210f2936da4fe591833e6f722
Kevin Locke <kevin@kevinlocke.name>
2021-01-26 10:13:12 -0700

    Replace Array#concat() with spread operator
    For readability and to satisfy the unicorn/prefer-spread rule.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- rules/ie11-restricted-globals.js
+++ rules/ie11-restricted-globals.js
@@ -9,22 +9,27 @@ const airbnbVariables =
 
 module.exports = {
   "rules": {
-    "no-restricted-globals": airbnbVariables
-      .rules["no-restricted-globals"]
-      // Remove IE-specific allowed globals
-      .filter((restriction) => {
-        // restriction can be string or object with .name property
-        // see https://eslint.org/docs/rules/no-restricted-globals#options
-        const name = restriction.name || restriction;
-        assert.strictEqual(typeof name, "string");
-        return true  // for consistent formatting below
-          // Allow globally since not supported on Number
-          && name !== "isFinite"
-          // Allow globally since not supported on Number
-          && name !== "isNaN";
-      })
+    "no-restricted-globals": [
+      "error",
+
+      // Airbnb restricted globals with IE-specific exceptions
+      ...airbnbVariables
+        .rules["no-restricted-globals"]
+        .slice(1)
+        .filter((restriction) => {
+          // restriction can be string or object with .name property
+          // see https://eslint.org/docs/rules/no-restricted-globals#options
+          const name = restriction.name || restriction;
+          assert.strictEqual(typeof name, "string");
+          return true  // for consistent formatting below
+            // Allow globally since not supported on Number
+            && name !== "isFinite"
+            // Allow globally since not supported on Number
+            && name !== "isNaN";
+        }),
+
       // Add globals not supported by IE 11
-      .concat([
+      ...[
         "Promise",
         "Proxy",
         "Reflect",
@@ -33,6 +38,7 @@ module.exports = {
       ].map((name) => ({
         name,
         "message": "Not supported by IE."
-      })))
+      }))
+    ]
   }
 };
--- rules/wsh-restricted-globals.js
+++ rules/wsh-restricted-globals.js
@@ -9,17 +9,22 @@ const notSupported = "Not supported by JScript.";
 
 module.exports = {
   "rules": {
-    "no-restricted-globals": ie11RestrictedGlobals
-      .rules["no-restricted-globals"]
-      // Adjust "Not supported by IE" messages for JScript
-      .map((restriction) => {
-        const { message } = restriction;
-        return message && message.startsWith("Not supported")
-          ? { ...restriction, "message": notSupported }
-          : restriction;
-      })
+    "no-restricted-globals": [
+      "error",
+
+      ...ie11RestrictedGlobals
+        .rules["no-restricted-globals"]
+        .slice(1)
+        // Adjust "Not supported by IE" messages for JScript
+        .map((restriction) => {
+          const { message } = restriction;
+          return message && message.startsWith("Not supported")
+            ? { ...restriction, "message": notSupported }
+            : restriction;
+        }),
+
       // Additional globals not supported by JScript
-      .concat([
+      ...[
         "constructor",
         "hasOwnProperty",
         "isPrototypeOf",
@@ -31,11 +36,13 @@ module.exports = {
       ].map((name) => ({
         name,
         "message": notSupported
-      })))
+      })),
+
       // WSH globals which are restricted
-      .concat({
+      {
         "name": "RuntimeObject",
         "message": "Use 'this' to get global object.  https://stackoverflow.com/q/3277182"
-      })
+      }
+    ]
   }
 };
