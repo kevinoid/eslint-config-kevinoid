08493afefc495a3917d33a335ebeaede68839a55
Kevin Locke <kevin@kevinlocke.name>
2021-02-03 10:03:07 -0700

    feat: Disable no-inner-declarations rule
    Since inner declarations are permitted by ES6 and the pre-ES6 engines I
    currently use (IE, WSH) allow such declarations, it's not a problem.  As
    with vars-on-top, stylistically I prefer such declarations where
    appropriate.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- rules/possible-errors.js
+++ rules/possible-errors.js
@@ -6,6 +6,12 @@
 module.exports = {
   "rules": {
     // disallow constant expressions in conditions
-    "no-constant-condition": "error"
+    "no-constant-condition": "error",
+
+    // allow function or variable declarations in nested blocks
+    // This is permitted in ES6 and accepted in all pre-ES6 engines I use.
+    // As with vars-on-top rule, prefer declarations in scope of use for
+    // ease of editing and comprehensibility.
+    "no-inner-declarations": "off"
   }
 };
