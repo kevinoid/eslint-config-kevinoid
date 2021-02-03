1895911c6c64d1e3a342ab3ccacc9af037b7fd2f
Kevin Locke <kevin@kevinlocke.name>
2021-02-03 10:59:09 -0700

    feat: enable no-import-assign
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- rules/possible-errors.js
+++ rules/possible-errors.js
@@ -12,6 +12,10 @@ module.exports = {
     // TODO [eslint-config-airbnb-base@>=15]: Remove if enabled
     "no-dupe-else-if": "error",
 
+    // disallow assigning to imported bindings
+    // TODO [eslint-config-airbnb-base@>=15]: Remove if enabled
+    "no-import-assign": "error",
+
     // allow function or variable declarations in nested blocks
     // This is permitted in ES6 and accepted in all pre-ES6 engines I use.
     // As with vars-on-top rule, prefer declarations in scope of use for
