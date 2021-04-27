6a6b525d1ffad8be15101b91c352d2eb60434a43
Kevin Locke <kevin@kevinlocke.name>
2021-04-27 17:43:53 -0600

    disable unicorn/prefer-module for this project
    This project can't be an ECMAScript Module because ESLint does not
    support ESM shareable configs yet.  Support is planned as part of the
    RFC 9 configuration system currently in development.  Until then,
    disable unicorn/prefer-module.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,12 @@ module.exports = {
     "comma-dangle": ["error", "never"],
     "max-len": "off",
     "quotes": ["error", "double"],
-    "quote-props": ["error", "always"]
+    "quote-props": ["error", "always"],
+
+    // ESLint Shareable Configs must be CommonJS
+    // (Until RFC 9 is implemented)
+    // https://github.com/eslint/eslint/issues/13440
+    // https://github.com/eslint/eslint/issues/13481
+    "unicorn/prefer-module": "off"
   }
 };
