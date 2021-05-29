dbbb624048977aadd1d99be9f639da84ce3c0cc7
Kevin Locke <kevin@kevinlocke.name>
2021-05-29 10:40:09 -0600

    note unicorn/prefer-node-protocol wait on depcheck
    It's probably preferable to hold off on this until also supported by
    depcheck.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- rules/unicorn.js
+++ rules/unicorn.js
@@ -101,8 +101,9 @@ module.exports = {
     "unicorn/prefer-dom-node-text-content": "off",
 
     // don't prefer node: protocol when importing builtin modules
-    // TODO: enable once eslint-plugin-node supports it:
+    // TODO: enable once eslint-plugin-node and depcheck support it:
     // https://github.com/mysticatea/eslint-plugin-node/issues/275
+    // https://github.com/depcheck/depcheck/pull/635
     "unicorn/prefer-node-protocol": "off",
 
     // don't prefer Number static properties over global ones.
