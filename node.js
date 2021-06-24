a01b279d971e4460719a76f0e42fe94d8efebd1c
Kevin Locke <kevin@kevinlocke.name>
2021-06-24 14:08:48 -0600

    feat: add nextTick to no-restricted-properties
    queueMicrotask() should be preferred to process.nextTick(), since it is
    standardized (by WHATWG HTML) and supported on both browsers and
    Node.js.
    
    BREAKING CHANGE: add nextTick to no-restricted-properties
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- node.js
+++ node.js
@@ -4,6 +4,8 @@
 "use strict";
 
 const requirePeer = require("./lib/require-peer.js");
+const { "rules": { "no-restricted-properties": noRestrictedProps } } =
+  require("./rules/best-practices.js");
 
 const { "configs": { "recommended": { "parserOptions": { sourceType } } } } =
   requirePeer("eslint-plugin-node");
@@ -61,6 +63,18 @@ module.exports = {
     // require 'use strict' in global scope
     // Note: This rule has no effect when sourceType: module
     "strict": ["error", "global"],
+
+    "no-restricted-properties": [
+      ...noRestrictedProps,
+      // Add process.nextTick() to no-restricted-properties
+      // https://nodejs.org/api/process.html#process_when_to_use_queuemicrotask_vs_process_nexttick
+      // https://github.com/sindresorhus/eslint-plugin-unicorn/issues/1346
+      {
+        "object": "process",
+        "property": "nextTick",
+        "message": "Use queueMicrotask() instead."
+      }
+    ],
 
     // Reports modules without any exports, or with unused exports
     // https://github.com/benmosher/eslint-plugin-import/blob/f63dd261809de6883b13b6b5b960e6d7f42a7813/docs/rules/no-unused-modules.md
