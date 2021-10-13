274bbd6b6756362f476adbbfdaaddc34c5148aa6
Kevin Locke <kevin@kevinlocke.name>
2021-10-13 11:46:27 -0600

    feat: enable typeof: true for no-undef rule
    Although the typeof operator can be used to test the presence of global
    variables, I suspect the likelihood and value of catching incorrect
    variable names outweighs the annoyance of disabling the rule or
    declaring global variables.
    
    Check that variables passed to `typeof` have been declared.
    
    BREAKING CHANGE: Configure `typeof: true` for `no-undef` rule.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- rules/variables.js
+++ rules/variables.js
@@ -8,6 +8,9 @@ module.exports = {
     // disallow shadowing of variables (including builtins and hoisted funcs)
     "no-shadow": ["error", { "builtinGlobals": true, "hoist": "all" }],
 
+    // disallow use of undeclared variables unless mentioned in a /*global */ block
+    "no-undef": ["error", { "typeof": true }],
+
     // disallow declaration of variables that are not used in the code
     // allow unused args for documentation, future use, and correct .length
     // allow in object destructuring to omit props from rest property
