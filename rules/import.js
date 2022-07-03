c10642a20135afe50ee25a7ce4374c9f6bcf97bc
Kevin Locke <kevin@kevinlocke.name>
2022-07-03 15:47:01 -0600

    feat: ignore import/no-unresolved false-positives
    As a temporary workaround for
    https://github.com/import-js/eslint-plugin-import/issues/1810
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- rules/import.js
+++ rules/import.js
@@ -3,10 +3,27 @@
 
 "use strict";
 
+const airbnbImports = require("eslint-config-airbnb-base/rules/imports");
+
+const airbnbNoUnresolvedOpts = airbnbImports.rules["import/no-unresolved"][1];
+
 module.exports = {
   "rules": {
     // Ensure consistent use of file extension within the import path
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
-    "import/extensions": ["error", "ignorePackages"]
+    "import/extensions": ["error", "ignorePackages"],
+
+    "import/no-unresolved": [
+      "error",
+      {
+        ...airbnbNoUnresolvedOpts,
+        "ignore": [
+          ...airbnbNoUnresolvedOpts.ignore ?? [],
+          // Ignore false positives due to issues with exports
+          // https://github.com/import-js/eslint-plugin-import/issues/1810
+          "ava"
+        ]
+      }
+    ]
   }
 };
