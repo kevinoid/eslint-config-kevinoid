kevinoid/eslint-config-kevinoid

commit 554ec3bc8d6c97e48092ab0354f83baa523842fc

feat: use eslint-plugin-regexp to check RegExps
Add dependencies on eslint-plugin-regexp and configure to match my
preferred style rules for regular expressions.

Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

ie11.js
@@ -23,6 +23,7 @@ module.exports = {
     // Plugin rules
     "./rules/jsdoc",
     "./rules/promise",
+    "./rules/regexp",
     "./rules/unicorn",
 
     // IE-specific plugin rules

wsh.js
@@ -26,6 +26,7 @@ module.exports = {
 
     // Plugin rules
     "./rules/jsdoc",
+    "./rules/regexp",
     "./rules/unicorn",
 
     // WSH-specific plugin rules

nodejs.js
@@ -26,6 +26,7 @@ module.exports = {
     "./rules/import",
     "./rules/jsdoc",
     "./rules/promise",
+    "./rules/regexp",
     "./rules/unicorn",
 
     // extend node plugin last because it sets parserOptions based on type

rules/ie11-regexp.js
@@ -1,0 +1,12 @@
+// ESLint IE 11 regexp configuration rules
+
+"use strict";
+
+module.exports = {
+  "rules": {
+    // Don't prefer/require features not supported by IE 11
+    "regexp/prefer-lookaround": "off",
+    "regexp/prefer-named-backreference": "off",
+    "regexp/prefer-result-array-groups": "off"
+  }
+};

rules/regexp.js
@@ -1,0 +1,76 @@
+// ESLint common configuration rules for Regexp plugin
+// https://github.com/ota-meshi/eslint-plugin-regexp#white_check_mark-rules
+
+"use strict";
+
+const requirePeer = require("../lib/require-peer.js");
+const warnToError = require("../lib/warn-to-error.js");
+
+const {
+  "configs": {
+    "all": {
+      rules,
+      ...regexpConfig
+    }
+  }
+} = requirePeer("eslint-plugin-regexp");
+
+// Configure all rules as error.
+const errorRules = warnToError(rules);
+
+// Remove deprecated rules
+delete errorRules["regexp/no-assertion-capturing-group"];
+delete errorRules["regexp/no-useless-exactly-quantifier"];
+delete errorRules["regexp/no-useless-non-greedy"];
+delete errorRules["regexp/order-in-character-class"];
+delete errorRules["regexp/prefer-t"];
+
+module.exports = {
+  ...regexpConfig,
+
+  "rules": {
+    ...errorRules,
+
+    // Don't require consistent letter case
+    // Capitalization of escape sequences is enforced by unicorn/escape-case.
+    // I don't want to enforce capitalization for case-insensitive queries,
+    // since I often find it preferable to use the common/expected case.
+    "regexp/letter-case": "off",
+
+    // Don't prefer \d to [0-9].
+    // I find 0-9 clearer. (\d matches Unicode digits in some RegEx dialects)
+    "regexp/prefer-d": "off",
+
+    // Don't prefer named capture groups.
+    // For simple cases and Array destructuring, unnamed is sufficient.
+    "regexp/prefer-named-capture-group": "off",
+
+    // Don't prefer replacements with named capture groups.
+    // This can be overkill for simple replacements.
+    "regexp/prefer-named-replacement": "off",
+
+    // Don't prefer quantifiers over repetition.
+    // Sometimes repeating the classes is clearer.
+    "regexp/prefer-quantifier": "off",
+
+    // Don't prefer \w to [0-9A-Za-z_].
+    // I find [0-9A-Za-z_] clearer. (\w matches Unicode in some RegEx dialects)
+    "regexp/prefer-w": "off",
+
+    // Don't require u flag
+    // There are enough cases where u is not useful (e.g. literal patterns,
+    // ASCII-only case-insensitive patterns) that it is counter-productive.
+    "regexp/require-unicode-regexp": "off",
+
+    // Don't require elements of character classes to be sorted.
+    // Although this is nice in theory, in practice I find sorting ASCII
+    // punctuation before numbers and letters to be more difficult to read.
+    // Also, placing - at the start/end to avoid escaping is nice.
+    "regexp/sort-character-class-elements": "off",
+
+    // Don't require the i flag to simplify expressions.
+    // Sometimes explicit is preferable.  It can also avoid unintentionally
+    // matching Unicode characters that case fold into the matched range.
+    "regexp/use-ignore-case": "off"
+  }
+};

webextensions.js
@@ -18,6 +18,7 @@ module.exports = {
     // Plugin rules
     "./rules/jsdoc",
     "./rules/promise",
+    "./rules/regexp",
     "./rules/unicorn"
   ],
 

commit 0532c9d900fd7fcc120cee3227dcf4a64cc914db

feat(ie11): disable unsupported regexp features
Disable rules which suggest/require RegExp features which are not
supported by IE.

I wrote these rules ages ago, but apparently forgot to include them in
the exported configurations.  Doh!

Fixes: 554ec3bc8d6c ("feat: use eslint-plugin-regexp to check RegExps")
Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

ie11.js
@@ -12,6 +12,7 @@ const globals = require("globals");
 
 // Note: IE11 doesn't support most ES6 features.  Use legacy ruleset.
 const commonLegacy = require("./common-legacy.js");
+const ie11Regexp = require("./rules/ie11-regexp.js");
 const ie11RestrictedGlobals = require("./rules/ie11-restricted-globals.js");
 const ie11RestrictedProperties = require("./rules/ie11-restricted-properties.js");
 const ie11RestrictedSyntax = require("./rules/ie11-restricted-syntax.js");
@@ -94,6 +95,7 @@ module.exports = [
   rulesPromise,
 
   // IE-specific plugin rules
+  ie11Regexp,
   ie11Unicorn,
 
   ie11Config

wsh.js
@@ -9,6 +9,7 @@ const { es5, wsh } = require("globals");
 // Note: WSH doesn't support ES5 or ES6.  Use legacy ruleset.
 const commonLegacy = require("./common-legacy.js");
 const airbnbStyle = require("./eslint-config-airbnb-base/rules/style.js");
+const ie11Regexp = require("./rules/ie11-regexp.js");
 const ie11Unicorn = require("./rules/ie11-unicorn.js");
 const wshRestrictedGlobals = require("./rules/wsh-restricted-globals.js");
 const wshRestrictedProperties = require("./rules/wsh-restricted-properties.js");
@@ -99,6 +100,7 @@ module.exports = [
   wshRestrictedSyntax,
 
   // Apply IE-specific plugin rules workable for WSH
+  ie11Regexp,
   ie11Unicorn,
 
   wshConfig